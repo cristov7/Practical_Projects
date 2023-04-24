f629b611eaccc7b6fb6a1b93b67aaa489da513fb
cristov_7 <114032977+cristov7@users.noreply.github.com>
2023-04-25 02:11:14 +0300

    Practical Project
    Practical Project

--- Guess A Number/Guess A Number.py
+++ Guess A Number/Guess A Number.py
@@ -6,17 +6,26 @@ print("""
              "GUESS A NUMBER"
 
 """)
+
+
 computer_number = random.randint(1, 100)
 count_attempts = 0
+
+
 yes_list = ["y", "yes"]
 no_list = ["n", "no"]
+
+
 while True:
     player_number = input("Guess the number (1-100): ")
+
     if not player_number.isdigit():
-        print("Invalid input. Try again...")
+        print("Invalid input. Try again...\n\n")
+
     else:
         player_number = int(player_number)
         count_attempts += 1
+
         if player_number == computer_number:
             print(f"Number of attempts: {count_attempts}\nYou guess it!")
             print("""
@@ -24,21 +33,23 @@ while True:
              CONGRATULATIONS!
              
             """)
-            command = input("Type [yes] to Play Again or [no] to quit: ")
-            if command.lower() in yes_list:
+
+            command = input("Type [yes] to Play Again or [no] to quit: ").lower()
+            if command in yes_list:
                 continue
-            elif command.lower() in no_list:
+
+            elif command in no_list:
                 print("""
                 
              THANK YOU FOR PLAYING!
                 """)
                 break
+
             else:
-                # print("Invalid Input. Try again...")
-                # break
                 raise SystemExit("Invalid Input. Try again...")
+
         elif player_number > computer_number:
             print(f"Number of attempts: {count_attempts} -> Too High!\n\n")
-        # elif player_number < computer_number:
-        else:
+
+        elif player_number < computer_number:
             print(f"Number of attempts: {count_attempts} -> Too Low!\n\n")
