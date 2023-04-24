b4ac7d594b905b13f204d767c90f454fb9c69a05
cristov_7 <114032977+cristov7@users.noreply.github.com>
2023-04-24 15:52:15 +0300

    Practical Project
    Practical Project

--- Random Sentences Generator/Random Sentences Generator.py
+++ Random Sentences Generator/Random Sentences Generator.py
@@ -1,36 +1,53 @@
 import random
 
 
-def get_random_word(words: list):
-    return random.choice(words)
+def get_random_word(words_list: list):
+    word = random.choice(words_list)
+    return word
 
-  
+
 print("""
                        WELCOME TO MY:
-                "RANDOM SENTENCES GENERATOR"
+                "RANDOM SENTENCES GENERATOR"  
 
 """)
-names_list = ["Peter", "Michell", "Jane", "Steve"]
-places_list = ["Sofia", "Plovdiv", "Varna", "Burgas"]
-verbs_list = ["eats", "holds", "sees", "plays with", "brings"]
-nouns_list = ["stones", "cake", "apple", "laptop", "bikes"]
-adverbs_list = ["slowly", "diligently", "warmly", "sadly", "rapidly"]
-details_list = ["near the river", "at home", "in the park"]
-print("Hello this is you first random sentence:")
-while True:
-    random_name = get_random_word(names_list)
-    random_place = get_random_word(places_list)
-    random_verb = get_random_word(verbs_list)
-    random_noun = get_random_word(nouns_list)
-    random_adverb = get_random_word(adverbs_list)
-    random_detail = get_random_word(details_list)
-    print(f"{random_name} from {random_place} {random_adverb} {random_verb} {random_noun} {random_detail}.\n\n")
-    command = input("Click [Enter] to generate new one.")
-    if command == "":
-        continue
-    else:
-        print("""
+
+
+first_names_list = ["Cristiano", "Lionel", "Robert"]
+last_names_list = ["Ronaldo", "Messi", "Lewandowski"]
+
+born_dates_list = ["05.February.1985", "24.June.1987", "21.August.1988"]
+born_places_list = ["Madeira, Portugal", "Rosario, Argentina", "Warsaw, Poland"]
+
+football_leagues_list = ["Premier League", "La Liga", "Bundesliga"]
+club_teams_list = ["Manchester United", "Barcelona", "Bayern Munich"]
+national_teams_list = ["Portugal", "Argentina", "Poland"]
+
+player_positions_list = ["winger", "forward", "striker"]
+player_numbers_list = [7, 10, 9]
+
+
+first_name = get_random_word(first_names_list)
+last_name = get_random_word(last_names_list)
+
+born_date = get_random_word(born_dates_list)
+born_place = get_random_word(born_places_list)
+
+football_league = get_random_word(football_leagues_list)
+club_team = get_random_word(club_teams_list)
+national_team = get_random_word(national_teams_list)
+
+player_position = get_random_word(player_positions_list)
+player_number = get_random_word(player_numbers_list)
+
+
+print(f"This is {first_name} {last_name}."
+      f"\nHe was born on {born_date} in {born_place}."
+      f"\n{last_name} is a captain both {football_league} club {club_team} and the {national_team} national team."
+      f"\nDefinitely the greatest {player_position} ever who plays with number {player_number}!")
+
+
+print("""
 
               THANK YOU FOR USING MY GENERATOR!
-        """)
-        break
+""")
